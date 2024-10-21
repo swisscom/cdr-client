e8c701ab5c7d014cb2466f488c2b2dbf4e0941d9
Ralf Steppacher <ralf.steppacher@swisscom.com>
2024-10-21 12:26:04 +0200

    Disable check for noop span implementation.

--- src/main/kotlin/com/swisscom/health/des/cdr/clientvm/TraceSupport.kt
+++ src/main/kotlin/com/swisscom/health/des/cdr/clientvm/TraceSupport.kt
@@ -18,9 +18,12 @@ object TraceSupport {
 
         val span: Span = tracer.spanBuilder().name(spanName).start()
 
-        require(span.toString().startsWith("PropagatedSpan")) {
-            "Expected an OpenTelemetry `PropagatedSpan` (which is a noop implementation) but got a ${span.javaClass.canonicalName} instead."
-        }
+        // TODO: Comment out for the time being. It appears the Azure Application Insights agent enables telemetry collection,
+        //   which causes below test to fail. We need to investigate whether the unclosed span (we close the "Span in Scope" but
+        //   not the span itself) causes a resource leak and if so, find another solution. See #37831.
+//        require(span.toString().startsWith("PropagatedSpan")) {
+//            "Expected an OpenTelemetry `PropagatedSpan` (which is a noop implementation) but got a ${span.javaClass.canonicalName} instead."
+//        }
 
         val spanInScope: SpanInScope = tracer.withSpan(span)
 
