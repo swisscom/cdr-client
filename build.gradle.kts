fc5c7395ac8026579ab1c006d3a548effe0233dd
Ralf Steppacher <ralf.steppacher@swisscom.com>
2025-02-12 11:47:34 +0100

    Bumped version to 3.3.0 (FINAL)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.net.URI
 import java.time.Duration
 
 group = "com.swisscom.health.des.cdr"
-version = "3.2.3-SNAPSHOT"
+version = "3.3.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 val awaitilityVersion: String by project
