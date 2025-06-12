8725a8c2a489d3ad81075d18362698ec77df32af
rsteppac <ralf@steppacher.name>
2025-06-12 11:17:58 +0200

    Update build.gradle.kts
    Co-authored-by: Lesrac <Lesrac@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
 }
 
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times in each subproject's classloader
+    // 'apply false' is necessary to avoid the plugins to be loaded multiple times in each subproject's classloader
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
